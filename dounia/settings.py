2c352c8b059a22d9d03bca3b749cb5b7dfde0efc
theodoreJordan <theodorejordan00@gmail.com>
2026-02-22 20:02:12 +0100

    fixing manual upload

--- dounia/settings.py
+++ dounia/settings.py
@@ -138,8 +138,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', BASE_DIR / 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/app/media'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
